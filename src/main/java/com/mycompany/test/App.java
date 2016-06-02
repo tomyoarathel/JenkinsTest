72793295b2f4abc453008ec54b766cc83dd263ea
Tomy <tomyoarathel@yahoo.com>
2016-06-02 15:07:09 -0400

    initial import

--- src/main/java/com/mycompany/test/App.java
+++ src/main/java/com/mycompany/test/App.java
@@ -6,7 +6,7 @@ package com.mycompany.test;
  */
 public class App 
 {
-    public static void main( String[] args )
+    blic static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
